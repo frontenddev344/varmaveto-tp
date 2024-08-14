3929ebab10b74ea59dddf39889b2ccd4fa6b8237
surinder <surinder.avology@gmail.com>
2024-08-14 15:20:39 +0530

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,34 @@
+// $(".toggle").click(function(){
+
+//   $("header").toggleClass("menuToggle");
+
+// });
+
+$(".toggle").click(function() {
+    $("header").addClass("menuToggle");
+});
+$(".close").click(function() {
+    $("header").removeClass("menuToggle");
+});
+
+
+
+
+
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 30) {
+        $('header').addClass('fixed-header');
+        $('header').addClass('visible-title');
+    }
+    else {
+        $('header').removeClass('fixed-header');
+        $('header').removeClass('visible-title');
+    }
+});
+
+    
+
+
+AOS.init({
+    duration: 1200,
+  })
